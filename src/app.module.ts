9ca4ad076c6499eb4b07812dec4952f15bbfd761
Vasyl <vasylpetryna2002@gmail.com>
2022-06-01 16:12:24 +0300

    Init Nest.js project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, AuthService],
+})
+export class AppModule {}

7306d6746feb8f9f470e75f7ff0981b28dcd5365
Vasyl <vasylpetryna2002@gmail.com>
2022-06-09 11:26:15 +0300

    Add redux logic to auth components

--- src/auth/types/index.ts
+++ src/auth/types/index.ts
@@ -2,4 +2,4 @@ export * from './tokens.types';
 export * from './jwt-payload.types';
 export * from './jwt-payload-with-rt.types';
 export * from './confirmation-token.types';
-export * from './auth-response.types'
+export * from './auth-response.types';
